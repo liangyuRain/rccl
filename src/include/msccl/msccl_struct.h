d39186249538d70315e430a5a7ce7e42de0be514
Liangyu <liangyu@cs.washington.edu>
2023-07-28 16:33:25 -0700

    multisend/recv compile passed

--- src/include/msccl/msccl_struct.h
+++ src/include/msccl/msccl_struct.h
@@ -19,6 +19,7 @@
 #define MSCCL_MAX_COUNT 72 // max concurrent number of msccl chunk transmission
 #define MSCCL_MAX_REDUCE_FUSION 16
 #define MSCCL_MAX_NUM_ALGOS 1024
+#define MSCCL_MAX_SEND_RECV_PEERS 8
 
 #define MSCCL_SLICESTEPS (NCCL_STEPS/4)
 #define MSCCL_CHUNKSTEPS (NCCL_STEPS/2)
@@ -52,14 +53,14 @@ struct mscclTransmission {
 
 static_assert((1ULL << (8*sizeof(mscclTransmission::count))) - 1 > MSCCL_MAX_COUNT, "MSCCL_MAX_COUNT must representable by datatype of count");
 
-struct mscclThreadBlock {
+struct alignas(sizeof(uint64_t)) mscclThreadBlock {
   // step is used to index into these arrays
   struct mscclTransmission transmissions[MSCCL_MAX_NUM_STEPS]; // 4KB
   int8_t dependentBid[MSCCL_MAX_NUM_STEPS]; // -1 if not dependent on any thread block, 256 bytes
   int16_t dependentStep[MSCCL_MAX_NUM_STEPS]; // 512 bytes
   int16_t reductionSrcOffsets[MSCCL_MAX_NUM_STEPS]; // 512 bytes
-  int16_t sendPeer;
-  int16_t recvPeer;
+  int16_t sendPeers[MSCCL_MAX_SEND_RECV_PEERS];
+  int16_t recvPeers[MSCCL_MAX_SEND_RECV_PEERS];
   uint16_t nSteps;
   int16_t channelId; // associated channel. -1 indicates a thread block with only local copies
 }; // 5384 bytes
@@ -72,7 +73,7 @@ struct mscclFlag {
 };
 
 struct mscclChannelPeerInfo {
-  int peer;
+  int peers[MSCCL_MAX_SEND_RECV_PEERS];
   // nTransmissionsOfCount[i]: number of transmissions with count i (in terms of msccl chunks)
   int nTransmissionsOfCount[MSCCL_MAX_COUNT + 1];
   int existingCounts[MSCCL_MAX_COUNT + 1];
