cb109a1b802c6ab43d879d35292a7ffcecd1d0f4
Liangyu <liangyu@cs.washington.edu>
2023-08-01 21:00:36 -0700

    temp fix 4

--- src/collectives/device/msccl_kernel_impl.h
+++ src/collectives/device/msccl_kernel_impl.h
@@ -386,6 +386,8 @@ __device__ __forceinline__ void mscclRunInterpreter(
       case 5: // make sure all cases are handled for nsend <= MSCCL_MAX_SEND_RECV_PEERS
         mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<1, 5>>(comm, algo, work);
         break;
+      default:
+        mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<1, MSCCL_MAX_SEND_RECV_PEERS>>(comm, algo, work);
     }
   } else if (nsend == 1) {
     switch (nrecv) {
@@ -404,6 +406,8 @@ __device__ __forceinline__ void mscclRunInterpreter(
       case 5:
         mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<5, 1>>(comm, algo, work);
         break;
+      default:
+        mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<MSCCL_MAX_SEND_RECV_PEERS, 1>>(comm, algo, work);
     }
   } else {
     mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<MSCCL_MAX_SEND_RECV_PEERS, MSCCL_MAX_SEND_RECV_PEERS>>(comm, algo, work);
