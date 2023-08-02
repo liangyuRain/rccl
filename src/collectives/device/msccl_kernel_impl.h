918b9e400040f4a0a716ace3baab250fba5de9e2
Liangyu <liangyu@cs.washington.edu>
2023-08-02 09:38:19 -0700

    add back default case

--- src/collectives/device/msccl_kernel_impl.h
+++ src/collectives/device/msccl_kernel_impl.h
@@ -383,9 +383,9 @@ __device__ __forceinline__ void mscclRunInterpreter(
       case 4:
         mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<1, 4>>(comm, algo, work);
         break;
-      // case 5: // make sure all cases are handled for nsend <= MSCCL_MAX_SEND_RECV_PEERS
-      //   mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<1, 5>>(comm, algo, work);
-      //   break;
+      default: // make sure all cases are handled for nsend <= MSCCL_MAX_SEND_RECV_PEERS
+        mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<1, MSCCL_MAX_SEND_RECV_PEERS>>(comm, algo, work);
+        break;
     }
   } else if (nsend == 1) {
     switch (nrecv) {
@@ -401,9 +401,9 @@ __device__ __forceinline__ void mscclRunInterpreter(
       case 4:
         mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<4, 1>>(comm, algo, work);
         break;
-      // case 5:
-      //   mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<5, 1>>(comm, algo, work);
-      //   break;
+      default:
+        mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<MSCCL_MAX_SEND_RECV_PEERS, 1>>(comm, algo, work);
+        break;
     }
   } else {
     mscclRunInterpreterHelper<T, RedOp, Proto, FanAsymmetric<MSCCL_MAX_SEND_RECV_PEERS, MSCCL_MAX_SEND_RECV_PEERS>>(comm, algo, work);
