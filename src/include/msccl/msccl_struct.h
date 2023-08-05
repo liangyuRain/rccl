0e00c6a07cd59b822f71802733ab9e4607916678
Liangyu <liangyu@cs.washington.edu>
2023-08-04 19:43:59 -0700

    max peer to 6; last commit normal

--- src/include/msccl/msccl_struct.h
+++ src/include/msccl/msccl_struct.h
@@ -19,7 +19,7 @@
 #define MSCCL_MAX_COUNT 72 // max concurrent number of msccl chunk transmission
 #define MSCCL_MAX_REDUCE_FUSION 16
 #define MSCCL_MAX_NUM_ALGOS 1024
-#define MSCCL_MAX_SEND_RECV_PEERS 5
+#define MSCCL_MAX_SEND_RECV_PEERS 6
 
 #define MSCCL_SLICESTEPS (NCCL_STEPS/4)
 #define MSCCL_CHUNKSTEPS (NCCL_STEPS/2)
