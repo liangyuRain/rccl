8d6d0b909a991d0be25548ad29ac3e0220b139b4
Liangyu <liangyu@cs.washington.edu>
2023-08-08 13:29:32 -0700

    const fan; last commit LL 300+us

--- src/collectives/device/prims_ll.h
+++ src/collectives/device/prims_ll.h
@@ -25,8 +25,7 @@ class Primitives<T, RedOp, Fan, Direct, ProtoLL, P2p>:
   const int wid;
   const int group;
   const int stepLines;
-  const int nrecv;
-  const int nsend;
+  const Fan fan;
   T *userBufs[2];
   struct ncclConnInfo* recvConn = NULL;
   volatile uint64_t* recvConnHeadPtr = NULL;
@@ -183,14 +182,17 @@ private:
 
   template<int BeginIx>
   __device__ void readLLBeginAll(int offset, ncclLLFifoLine(&line)[MaxRecv]) {
-    for (int i=BeginIx; i < nrecv; i++) {
-      union ncclLLFifoLine* src = recvPtr(i) + offset;
+    #pragma unroll
+    for (int i=BeginIx; i < MaxRecv; i++) {
+      if (i < fan.nrecv()) {
+        union ncclLLFifoLine* src = recvPtr(i) + offset;
 #if defined(__HIP_PLATFORM_HCC__) || defined(__HCC__) || defined(__HIPCC__)
-      line[i].v[0] = __builtin_nontemporal_load(src->v);
-      line[i].v[1] = __builtin_nontemporal_load(src->v+1);
+        line[i].v[0] = __builtin_nontemporal_load(src->v);
+        line[i].v[1] = __builtin_nontemporal_load(src->v+1);
 #else
-      asm("ld.volatile.global.v4.u32 {%0,%1,%2,%3}, [%4];" : "=r"(line[i].data1), "=r"(line[i].flag1), "=r"(line[i].data2), "=r"(line[i].flag2) : "l"(&src->i4));
+        asm("ld.volatile.global.v4.u32 {%0,%1,%2,%3}, [%4];" : "=r"(line[i].data1), "=r"(line[i].flag1), "=r"(line[i].data2), "=r"(line[i].flag2) : "l"(&src->i4));
 #endif
+      }
     }
   }
   __device__ uint64_t readLLFinish(int offset, ncclLLFifoLine(&line)[MaxRecv], int i) {
@@ -423,7 +425,8 @@ private:
       }
       if (RECV) {
         data = !SRC ? peerData : applyReduce(redOp, peerData, data);
-        for (int i=1; i < nrecv; i++) {
+        #pragma unroll MaxRecv
+        for (int i=1; i < MaxRecv && i < fan.nrecv(); i++) {
           peerData = readLLFinish(offset, line, i);
           data = applyReduce(redOp, peerData, data);
         }
@@ -433,7 +436,7 @@ private:
 
       // Send : inter-node, then intra-node, then local
       if (SEND) {
-        for (int i=1; i < nsend; i++)
+        for (int i=1; i < MaxSend && i < fan.nsend(); i++)
           storeLL(sendPtr(i)+offset, data, sendFlag(i));
         storeLL(sendPtr(0)+offset, data, sendFlag(0));
       }
@@ -461,13 +464,11 @@ private:
 #endif
 
     if (RECV) {
-      // MaxRecv is at least 1
-      incRecv(0);
-      for (int i=1; i < nrecv; i++) incRecv(i);
+      for (int i=0; i < MaxRecv; i++) incRecv(i);
       postRecv();
     }
     if (SEND) {
-      for (int i=1; i < nsend; i++)
+      for (int i=1; i < MaxSend && i < fan.nsend(); i++)
         incSend(i, offset);
       incSend(0, offset);
     }
@@ -542,7 +543,7 @@ private:
     if (wid == i) recvConn = conn;
   }
   __device__ __forceinline__ void loadRecvSync() {
-    if (tid >= nthreads-WARP_SIZE && wid < nrecv) {
+    if (tid >= nthreads-WARP_SIZE && wid < fan.nrecv()) {
       recvConnHeadPtr = recvConn->head;
       recvConnHead = recvConn->step;
     }
@@ -554,7 +555,7 @@ private:
     if (wid == i) sendConn = conn;
   }
   __device__ __forceinline__ void loadSendSync() {
-    if (tid < nsend) {
+    if (tid < fan.nsend()) {
       sendConnHeadPtr = sendConn->head;
       sendConnHeadCache = *sendConnHeadPtr;
       sendConnHead = sendConn->step;
@@ -571,7 +572,7 @@ private:
     redOp(redOpArg),
     tid(tid), nthreads(nthreads), wid(tid%WARP_SIZE), group(group),
     stepLines(ncclShmem.comm.buffSizes[NCCL_PROTO_LL]/NCCL_STEPS/sizeof(ncclLLFifoLine)),
-    nrecv(nrecv), nsend(nsend) {
+    fan(nrecv, nsend) {
     auto *channel = &ncclShmem.channel;
     barriers = &ncclShmem.groups[group].barrier;
     barrier_next = ncclShmem.groups[group].barrier_next;
@@ -590,9 +591,9 @@ private:
 
   __device__ ~Primitives() {
     // Save steps for the next operation
-    if (tid >= nthreads-WARP_SIZE && wid < nrecv)
+    if (tid >= nthreads-WARP_SIZE && wid < fan.nrecv())
       recvConn->step = recvConnHead;
-    if (tid < nsend)
+    if (tid < fan.nsend())
       sendConn->step = sendConnHead;
     // Ensure all steps written back
     barrier();
